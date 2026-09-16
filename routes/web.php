a912f24bb680dd1925060162404d1f54c6e78d48
Nicolas BOUTINAUD <nicolas@boutinaud.ch>
2026-09-17 01:15:33 +0200

    test correction 1

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,32 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ItemLocaleController;
 use App\Http\Controllers\EmblemController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ShipController;
 use App\Http\Controllers\SparrowController;
-use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});*/
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/sparrows', [SparrowController::class, 'index'])->name('sparrows.index');
 Route::get('/ships', [ShipController::class, 'index'])->name('ships.index');
 Route::get('/emblems', [EmblemController::class, 'index'])->name('emblems.index');
 
-Route::post('/locale', function (\Illuminate\Http\Request $request) {
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::post('/locale', function (Request $request) {
     $locale = $request->input('locale', 'fr');
 
     $request->session()->put('locale', $locale);
